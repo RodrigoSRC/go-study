5957aef198763c5323da5af1ca1a364a8d508553
Rodrigo <clancoto2015@hotmail.com>
2026-06-27 11:06:49 -0300

    Add main function and error handling for file opening

--- aula1/main.go
+++ aula1/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+) 
+
+func main() {
+	fmt.Println("Hello, World!")
+	_, err := os.Open("arquivo.txt")
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo:", err)
+		return
+	}
+	// defer arq.Close()
+	// arq, err = os.Open("arquivo.txt")
+
+}
+
+func init() {
+	fmt.Println("Init")
+}
