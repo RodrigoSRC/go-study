c4d37692dc9b76f984bf9446c7449967cd85752b
Rodrigo <clancoto2015@hotmail.com>
2026-06-27 11:34:09 -0300

    Refactor file reading logic into a separate function and improve error messages

--- aula1/main.go
+++ aula1/main.go
@@ -5,16 +5,24 @@ import (
 	"os"
 ) 
 
-func main() {
-	fmt.Println("Hello, World!")
-	_, err := os.Open("arquivo.txt")
+func lerArquivo(nome string) (*os.File, error) {
+	arq, err := os.Open(nome)
 	if err != nil {
-		fmt.Println("Erro ao abrir o arquivo:", err)
+		return nil, err
+	}
+	defer arq.Close()
+	return arq, nil
+}
+
+func main() { 
+	fmt.Println("Hello, World!")
+	_, err := lerArquivo("rotina.go")
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
 		return
 	}
-	// defer arq.Close()
-	// arq, err = os.Open("arquivo.txt")
 
+	fmt.Println("Arquivo lido com sucesso")
 }
 
 func init() {
