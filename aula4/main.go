12bcd66241995b7df45258fcc62925fdfdf35bc8
Rodrigo <clancoto2015@hotmail.com>
2026-07-02 11:44:06 -0300

    Add SortearIdade function and update CriarArquivoGb to generate random ages and corresponding data entries

--- aula4/main.go
+++ aula4/main.go
@@ -5,10 +5,23 @@ import (
 	lib "github.com/aoticombr/golang/lib"
 )
 
+func SortearIdade(ate int ) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(ate)
+}
+
 func CriarArquivoGb(value int) {
 	var arquivo cp.String
 
 	arquivo.Add("Nome | Idade | Dta Nasci | Logradouro | Bairro | Cidade")
+
+	for i := 0; i < 10000000*value; i++ {
+		Idade:= SortearIdade(100)
+		Data:= time.Now()
+		Data = Data.AddDate(0, 0, -Idade)
+		linha:= fmt.Sprintf("Nome %d | %d | %s | Rua %d | Bairro %d | Cidade %d", i, Idade, Data.Format("02/01/2006"), i, i, i)
+		arquivo.Add(linha)
+	}
 
 	err:= lib.ByteToSaveFile("aula4.txt", arquivo.Bytes())
 	if err != nil {
@@ -18,6 +31,7 @@ func CriarArquivoGb(value int) {
 }
 
 func main() {
+	CriarArquivoGb(2)
 
 
 	// fmt.Println("Arquivo salvo com sucesso!")
